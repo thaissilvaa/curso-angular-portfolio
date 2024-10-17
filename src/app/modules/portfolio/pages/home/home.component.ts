706c69be5237b9e061a57e6d73334086b1822031
Thais Silva <tatahcristina13@gmail.com>
2024-10-17 15:48:33 -0300

    Criando a header da página (imagens não abrem)

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, 
+    HeaderComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
